96b4e088a5349182edd41ee4fe2b1bfebf318497
rdmouhouadi <rd.mouhouadi@gmail.com>
2025-08-12 21:58:19 +0200

    setting the local repo with class and main  files

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Item:
+    def name(self):
+        '''a method that returns the item name'''
+        pass
+
+    def weight(self):
+        '''a method that returns the item weight'''
+        pass
